f94a3979eb5e6cc7d88bf8f7c3d3057943f273f5
zou111223 <3541875464@qq.com>
2025-07-30 08:22:26 +0800

    Create UserCenter.vue

---
+++ UserCenter.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <el-card>
+      <h2>个人信息</h2>
+      <div>用户名：{{ user.username }}</div>
+      <div>手机号：{{ user.phone }}</div>
+      <div>地址：{{ user.address }}</div>
+    </el-card>
+  </div>
+</template>
+<script setup>
+import { ref, onMounted } from 'vue'
+import { getUserInfo } from '../api/user'
+const user = ref({})
+onMounted(async () => {
+  const res = await getUserInfo()
+  user.value = res.data
+})
+</script>
